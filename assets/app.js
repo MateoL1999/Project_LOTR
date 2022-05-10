601a385910b2bd4387ad17ce25fb69b261f06017
MateoL1999 <mateoleiros@gmail.com>
2022-05-10 20:24:19 +0200

    premier jour quasi fin du header responsive

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,7 @@
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
 
+import './styles/js/script.js';
+
 // start the Stimulus application
 import './bootstrap';
---
+++ assets/styles/js/script.js
@@ -0,0 +1,23 @@
+const nav = document.querySelector("nav");
+const menu = document.querySelector('.menu');
+const burger = document.querySelector('.burger');
+console.log('kuruma')
+
+let lastScroll = 0;
+
+burger.addEventListener("click", () => {
+    menu.classList.toggle("menu");
+});
+
+
+
+
+
+// window.addEventListener("scroll", () => {
+//     if (window.scrollY < lastScroll) {
+//         nav.style.top = 0;
+//     } else {
+//         nav.style.top = "-60px";
+//     }
+//     lastScroll = window.scrollY;
+// });
