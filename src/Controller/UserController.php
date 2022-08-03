721e3c4283be5a41e021e6b51c1b49aa7dc9c8da
MateoL1999 <mateoleiros@gmail.com>
2022-08-03 20:01:39 +0200

    Commentaire accessible et responsive finit attention mettre beaucoup de texte problème avec les users dans les com.
    Début Profil (controller fait)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -50,6 +50,19 @@ class UserController extends AbstractController
         ]);
     }
 
+    #[Route('/users/{id}/profil', name: 'user_profil')]
+    public function userProfil($id, UserRepository $userRepository, Request $request, EntityManagerInterface $entityManager)
+    {
+        $user = $userRepository->findOneBy(['id' => $id]);
+        if (!$user) {
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('user/userProfil.html.twig',[
+            'users' => $user
+    ]);
+    }
+
     #[Route('/user/{id}/delete',name: 'user_delete')]
     public function userDelete(UserRepository $userRepository, EntityManagerInterface $entityManager,$id )
     {
